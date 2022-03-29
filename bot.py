6b718b195c53bf4d0ee19cc55bd87f680bce8cd6
Wirexia-git <d20124630@mytudublin.ie>
2022-03-29 12:34:55 +0100

    Trying to set up login script

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from instabot import Bot
+
+COOKIE_FNAME = 'cookie.txt'
+
+bot = Bot()
+bot.login(username="wirexia66@gmail.com", password="reNcDjEgWp4X7Gt", use_cookie = False)
+user_id = bot.get_user_id_from_username("lego")
+user_info = bot.get_user_info(user_id)
+print(user_info['biography'])
